6538d2e0114100e4d3073751cc72a33108eced89
DobriJS <53184157+DobriJS@users.noreply.github.com>
2021-10-17 21:46:06 +0300

    fix route

--- src/server.ts
+++ src/server.ts
@@ -40,7 +40,7 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
 
-app.use('/users', userRoutes);
+app.use('/user', userRoutes);
 
 app.get("/", (req, res) => {
 	res.send("Hello to Widgets");
