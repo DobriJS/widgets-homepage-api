fa44938589724a3901fcf94dd875cce9631caf66
DobriJS <53184157+DobriJS@users.noreply.github.com>
2021-10-22 16:00:58 +0300

    Fix CORS

--- src/server.ts
+++ src/server.ts
@@ -38,7 +38,7 @@ app.use((req, res, next) => {
 });
 
 app.use(express.json());
-app.use(cors({ origin: 'https://homepage-widgets--pr42-axios-config-api-rx4ai3yg.web.app', credentials: true }));
+app.use(cors({ origin: 'https://homepage-widgets.web.app', credentials: true }));
 
 app.use('/user', userRoutes);
 
